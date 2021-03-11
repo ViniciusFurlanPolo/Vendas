8bbd35f41b7244c8e1e4b0ea21401c0519f54217
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2021-03-11 17:59:30 -0300

    VendasService

---
+++ src/main/java/com/vendas/api/services/VendasService.java
@@ -0,0 +1,59 @@
+package com.vendas.api.services;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vendas.api.entities.Funcionario;
+import com.vendas.api.entities.Venda;
+import com.vendas.api.repositories.FuncionarioRepository;
+import com.vendas.api.repositories.VendaRepository;
+import com.vendas.api.utils.ConsistenciaException;
+
+@Service
+public class VendasService {
+	
+	private static final Logger log = LoggerFactory.getLogger(VendasService.class);
+	
+	@Autowired
+	private VendaRepository vendaRepository;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	private Optional<Venda> buscarPorId(int id) throws ConsistenciaException {
+		log.info("Service: buscando uma venda de id: {}", id);
+		
+		Optional<Venda> venda = vendaRepository.findById(id);
+		
+		if(!venda.isPresent()) {
+			log.info("Service: Nenhuma venda com id: {} foi encontrado", id);
+			throw new ConsistenciaException("Nenhuma venda com id: {} foi encontrado", id);
+		}
+		
+		return null;
+	}
+	
+	public Venda salvar(Venda venda) throws ConsistenciaException, ParseException{
+		log.info("Service: salvando a Venda: {}", venda);
+		
+		Date now = new Date();
+		
+		Funcionario func = funcionarioRepository.findBynome(venda.getFuncionario().getNome());
+		
+		venda.setDataVenda(now);
+		venda.setId(func.getId());
+		
+		if(venda.getId() > 0)
+			buscarPorId(venda.getId());
+		
+		return vendaRepository.save(venda);
+	}
+
+
+}
