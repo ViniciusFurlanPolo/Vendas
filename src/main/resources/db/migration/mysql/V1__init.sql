e87fa6e342a93b49d58b53a78ad12652f0883098
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2021-03-10 21:48:07 -0300

    Aquivos DB

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,28 @@
+-- -----------------------------------------------------
+-- Table `funcioario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` CHAR(100) NOT NULL,
+  `total_de_vendas` INT NOT NULL,
+    PRIMARY KEY(`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `venda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `venda` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `data_da_venda` DATE NOT NULL,
+  `valor` DOUBLE NOT NULL,
+  `id_func` INT NOT NULL,
+  `nome_func` CHAR(100) NOT NULL,
+    PRIMARY KEY(`id`)
+    FOREIGN KEY(`id_func`)
+    REFERENCES `funcionario` (`id`)
+    ON DELETE NO ACTION
+    ON UPTADE NO ACTION)
+ENGINE = InnoDB;
