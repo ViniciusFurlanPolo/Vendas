761dde522f573a5d46e1480f42b0335ecae2efcb
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2021-03-11 17:05:54 -0300

    Correção VendaRepository

--- src/main/java/com/vendas/api/repositories/VendaRepository.java
+++ src/main/java/com/vendas/api/repositories/VendaRepository.java
@@ -3,6 +3,7 @@ package com.vendas.api.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vendas.api.entities.Venda;
@@ -10,6 +11,7 @@ import com.vendas.api.entities.Venda;
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
 	
 	@Transactional(readOnly = true)
+	@Query("SELECT fc FROM Venda fc WHERE fc.funcionario.id = :idFunc")
 	List<Venda> findByid_func(int idFunc);
 
 }
